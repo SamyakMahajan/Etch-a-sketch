edef4cc9a9bc63056a1f30cabbd8fef518710771
SamyakMahajan <ketann682@gmail.com>
2023-12-08 21:40:32 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,64 @@
+// script.js
+let erase=false
+document.addEventListener('DOMContentLoaded', () => {
+    const gridCells = document.querySelectorAll('.grid_cell');
+    let mousedown = false;
+    let chosenColor = "#5C08A0"; // Default color
+
+    const body = document.body;
+    colorPicker.addEventListener('input', () => {
+        // if(erase){
+        //     chosenColor="#ffffff";
+        // }
+        // else
+            chosenColor = colorPicker.value;
+        selectedColor.style.backgroundColor = chosenColor;
+    });
+    body.addEventListener('mousedown', () => {
+        mousedown = true;
+    });
+
+    body.addEventListener('mouseup', () => {
+        mousedown = false;
+    });
+
+    gridCells.forEach(cell => {
+        cell.addEventListener('mousedown', () => {
+            mousedown = true;
+            if(erase){
+                chosenColor="#fff";
+            }
+            else
+                chosenColor = colorPicker.value;
+        
+            cell.style.backgroundColor = chosenColor;
+        });
+
+        cell.addEventListener('mouseup', () => {
+            mousedown = false;
+        });
+
+        cell.addEventListener('mouseenter', () => {
+            if (mousedown) {
+                if(erase){
+                    chosenColor="#ffffff";
+                }
+                else
+                    chosenColor = colorPicker.value;
+            
+                    cell.style.backgroundColor = chosenColor;
+                }
+            
+        });
+    });
+});
+
+function toggleClickedState() {
+    let button = document.getElementById('Erasor');
+    erase=!erase
+    button.classList.toggle('clicked'); // Toggle the 'clicked' class
+  }
+
+  document.addEventListener('dragstart', function (event) {
+    event.preventDefault();
+  });
